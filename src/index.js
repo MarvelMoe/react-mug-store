cf727bd70df3ea8483746c2a6b86e84af69eec42
Moe <marvel451@aol.com>
2018-08-03 20:34:56 -0700

    Added Routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import {render} from "react-dom";
 import Router from "./components/Router";
-// import App from "./components/App";
-// import StorePicker from './components/StorePicker';
-
 import "./css/style.css";
 
 render(<Router/> ,document.getElementById("main"));
